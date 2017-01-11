0c5bcc900afa8063962044a8cb8aa1847de642e0
Andrew Linxie <alinxie@berkeley.edu>
2017-01-11 12:36:38 -0800

    Trying to do sillhouette score

--- exploration.py
+++ exploration.py
@@ -6,14 +6,3 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-x = [1,2,3,4,5]
-y = [1,2,3,4,5]
-plt.plot(x,y)
-
-def get_avg_comments():
-    
-def get_max_comments():
-
-def get_median_comments():
-
-def get_size_of_comment(comment_table_name)
